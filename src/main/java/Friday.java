8a6b7517ad9e8826cf56d118bd5d20f617be32b0
Thejus <thejusunni03@gmail.com>
2025-08-19 22:02:25 +0800

    Add Friday.java

---
+++ src/main/java/Friday.java
@@ -0,0 +1,7 @@
+public class Friday {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Friday");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
