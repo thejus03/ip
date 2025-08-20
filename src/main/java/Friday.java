1220d9bfbfd36fab2287813b82415a255c111b56
Thejus <thejusunni03@gmail.com>
2025-08-20 15:09:54 +0800

    Update Friday.java to have List feat

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Friday {
@@ -5,19 +7,25 @@ public class Friday {
         System.out.println("-".repeat(50));
         System.out.println("Hello! I'm Friday");
         System.out.println("What can I do for you?");
-        System.out.println("-".repeat(50));
+        List<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.println("-".repeat(50));
             String input = scanner.nextLine();
+            System.out.println("-".repeat(50));
             if (input.toLowerCase().equals("bye")) {
                 break;
+            } else if (input.toLowerCase().equals("list")){
+                System.out.println("Here are your tasks: ");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                System.out.println(input);
+                tasks.add(input);
             }
-            System.out.println("-".repeat(50));
-            System.out.println(input);
-            System.out.println("-".repeat(50));
         }
-        System.out.println("-".repeat(50));
         scanner.close();
 
         System.out.println("Bye. Hope to see you again soon!");
