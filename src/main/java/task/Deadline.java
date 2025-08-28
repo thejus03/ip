b573dbb88c2d753b21b264afcd5415593df6e749
Thejus <thejusunni03@gmail.com>
2025-08-28 20:25:25 +0800

    Add parseTime func to parse datetime for Deadline and Event

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,5 +1,9 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     private String by;
     private String desc;
@@ -10,7 +14,7 @@ public class Deadline extends Task {
         if (parts.length < 2) {
             throw new DeadlineArgsException("The deadline cannot be empty... it must have the 'by' part.");
         }
-        this.by = parts[1].trim();
+        this.by = parseTime(parts[1].trim());
 
     }
 
@@ -32,5 +36,14 @@ public class Deadline extends Task {
     public static String parseDesc(String desc) {
         String[] parts = desc.split("/by", 2);
         return parts[0].trim();
+    }
+
+    public static String parseTime(String by) {
+        try {
+            LocalDate date = LocalDate.parse(by);
+            return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            return by;
+        }
     }
 }
