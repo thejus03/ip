d27a4e0f45045d4dac5079b8d471fa72c5151803
Thejus <thejusunni03@gmail.com>
2025-08-29 12:28:41 +0800

    Add test files - Add DeadlineTest.java for Deadline tests - Add EventTest.java for Event tests

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,31 @@
+import friday.task.Deadline;
+import friday.task.DeadlineArgsException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DeadlineTest {
+    @Test
+    public void testDeadlineCreationValid() {
+        Deadline deadline = new Deadline("submit report /by 2023-10-15");
+        System.out.println(deadline.getFullDesc());
+        assertEquals("[D] [ ] submit report (by: Oct 15 2023)", deadline.toString());
+    }
+
+    @Test
+    public void testDeadlineCreationInvalid() {
+        try {
+            new Deadline("submit report");
+            fail();
+        } catch (DeadlineArgsException e) {
+            assertEquals("The deadline cannot be empty... it must have the 'by' part.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDeadlineCreationValidNoDate() {
+        Deadline deadline = new Deadline("submit report /by tomorrow");
+        assertEquals("[D] [ ] submit report (by: tomorrow)", deadline.toString());
+    }
+}
---
+++ src/test/java/EventTest.java
@@ -0,0 +1,25 @@
+import friday.task.EventArgsException;
+import org.junit.jupiter.api.Test;
+import friday.task.Event;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class EventTest {
+
+    @Test
+    public void eventCreationValid() {
+        Event e = new Event("career fair /from 2025-09-01 /to 2025-09-03");
+        assertEquals("[E] [ ] career fair (from: Sept 1 2025 to: Sept 3 2025)", e.toString());
+    }
+
+    @Test
+    public void eventCreationInvalid() {
+        try {
+            new Event("career fair /from 2025-09-01");
+            fail();
+        } catch (EventArgsException e) {
+            assertEquals("Event description must include '/to' part.", e.getMessage());
+        }
+    }
+}
