3c9fd99dd9156b3af45a1eb752959d935181722e
Thejus <thejusunni03@gmail.com>
2025-09-02 23:16:20 +0800

    Add GUI

--- src/main/java/friday/Friday.java
+++ src/main/java/friday/Friday.java
@@ -21,4 +21,11 @@ public class Friday {
     public static void main(String[] args) {
         new Friday().run();
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Friday heard: " + input;
+    }
 }
